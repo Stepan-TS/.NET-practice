f22e6d77cc855fcd1ca552647804310f0aa7f023
Stepan <102257376+Stepan-TS@users.noreply.github.com>
2022-11-08 12:25:45 +0200

    add files

---
+++ Task First/Company.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_First
+{
+    internal class Company
+    {
+        public Company(string name, Address address, Person director, int numberOfEmployees)
+        {
+            Name = name;
+            Address = address;
+            Director = director;
+            NumberOfEmployees = numberOfEmployees;
+        }
+        public string Name { get;  }
+        public Address Address { get; set; }
+        public Person Director { get; set; }
+
+        public int NumberOfEmployees { get; set; }
+
+
+        public string DisplayCompanyInfo()
+        {
+            return ("Name: " + Name + ";\n" + "Address of company: " + "\n" + Address.GetAddress() + ";\n" + "Information about director: " + "\n" + Director.DisplayPersonalInfo() + "\n" + "NumberOfEmployees: " + NumberOfEmployees + ";");
+        }
+
+        public void changeDirectory(Person newDirector) 
+        {
+            Director = newDirector;   
+        }
+        public void Hire(Person newPerson)
+        {
+             NumberOfEmployees = NumberOfEmployees+1;
+            if(newPerson.JobTitle == "director")
+            {
+                Director = newPerson;
+            }
+        }
+
+    }
+}
---
+++ Task First/Person.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Runtime.Loader;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_First
+{
+    internal class Person
+    {
+
+        public Person(string firstName, string lastName, int age, Address address, string jobTitle)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Age = age;
+            JobTitle = jobTitle;
+            Address = address;
+
+        }
+
+        
+
+        public string FirstName { get; }
+        public string LastName { get; }
+        public int Age { get; }
+        public Address Address { get; set; }
+        public string JobTitle { get; set; }
+
+
+        public string DisplayPersonalInfo()
+        {
+            return ("FirstName: " + FirstName + ";\n" + "LastName: " + LastName + ";\n" + "Age: " + Age + ";\n" + Address.GetAddress() + ";\n" + "JobTitle: " + JobTitle);
+        }
+        public void ChangeAddress(Address newAddress)
+        {
+            Address = newAddress;
+        }
+
+    }
+}
+    
